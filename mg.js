a5d0edbdd51563d1b5c73e3bc3428c70d3efd0be
ezzaldean1 <42187031+ezzaldean1@users.noreply.github.com>
2018-08-12 12:54:39 +0300

    Update mg.js

--- mg.js
+++ mg.js
@@ -4,7 +4,7 @@ client.on('ready', () => {
   console.log('---------------');
   console.log(' Bot Is Online')
   console.log('---------------')
-    client.user.setGame(` Beauty Is Skin Deep ヅ`, "http://twitch.tv/Streammingg")
+    client.user.setGame(` United Server ヅ`, "http://twitch.tv/Streammingg")
 
 });
 
